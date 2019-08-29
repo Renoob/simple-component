7d2d86c622c0ce93ad0a7113e43d9fd3664ef975
renoob <zekerjc@gmail.com>
2019-08-29 18:07:21 +0800

    修改结构

--- example/containers/countDown/index.tsx
+++ example/containers/countDown/index.tsx
@@ -1,5 +1,5 @@
-import * as React from "react";
 import CountDown from "@components/countDown";
+import * as React from "react";
 
 class CountDownExample extends React.Component {
     public render() {
