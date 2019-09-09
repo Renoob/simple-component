674d1429a79f2e3cbd48fe497e0d46df8e084d67
renoob <zekerjc@gmail.com>
2019-09-09 17:27:47 +0800

    解决ts-loader不能触发HMR热更新的问题

--- example/index.tsx
+++ example/index.tsx
@@ -7,7 +7,7 @@ const container = document.getElementById("container");
 ReactDOM.render(<App />, container);
 
 if (module.hot) {
-    module.hot.accept("@router/app.tsx", () => {
+    module.hot.accept("@router/main.tsx", () => {
         ReactDOM.render(<App />, container);
     });
 }
