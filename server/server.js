8e5884246a2eb451c1a5f38bc28a26cae2c307eb
renoob <zekerjc@gmail.com>
2019-09-23 16:42:42 +0800

    添加stylelint检查，添加npm-run-all运行多命令

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
-"use strict";
-exports.__esModule = true;
-var express = require("express");
-var app = express();
-var port = 8080;
-app.get("/", function (req, res) { return res.send("Hello World"); });
-app.listen(port, function () { return console.log("Example app listening on port " + port + "!"); });
+import express from "express";
+const app = express();
+const port = 8080;
+
+app.get("/", (req, res) => res.send("Hello World"));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
